b1fc1f136d720943d027ba6151d86650b1755f78
Ray Cruz <raycruz@quantumwizad.com>
2021-01-28 17:00:58 -0800

    added module selector program

--- exams/exam_2/exam_2.php
+++ exams/exam_2/exam_2.php
@@ -1,6 +1,8 @@
 <?php
 include '../../src/exam_manager.php';
 
+fixed_order();
+
 $students = array(1234=>"Steve Rogers", 4321=>"Tony Stark");
 
 $problems = array(
@@ -12,14 +14,24 @@ $problems = array(
 
 $summary = array();
 
-$randseed = "testin";
+//A given $randseed should produce deterministic output for a given student
+$randseed = __FILE__;
 
-print_exams($students, $problems, $summary);
+$summary1 = print_exams($students, $problems);
 
-print_summary($summary);
+print_summary($summary1);
 
-print_solutions($students, $problems, $summary);
+$summary2 = print_solutions($students, $problems, $summary);
 
-print_summary($summary)
+print_summary($summary2);
+
+//sanity check
+if (json_encode($summary1)!=json_encode($summary2)){
+    echo "The is an issue, exams and solutions are not the same!\n";
+} else {
+    echo "Successfully generated exams and solutions\n";
+}
+
+
 
 ?>
